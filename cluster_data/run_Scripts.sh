a1e50a75feeded8681dc77b4bf2dfaf2266dadb5
Alblahm <alblahm@hotmail.com>
2020-04-20 20:47:46 +0200

    Last Update

---
+++ cluster_data/run_Scripts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script del shell realizado por Victor Leboran
+# Para mas info contacta en victor.leboran@usc.es
+
+
+./runsimular_paral -2 0 300 "./Madrid/ParInit_Optimos_08_006_Exp4.txt" Madrid
+
+
