711895ea28bde11b8a7860054af9661841e5be45
Alblahm <alblahm@hotmail.com>
2020-04-21 02:00:30 +0200

    Actualizados datos Madrid

--- cluster_data/run_Scripts.sh
+++ cluster_data/run_Scripts.sh
@@ -2,7 +2,7 @@
 # Script del shell realizado por Victor Leboran
 # Para mas info contacta en victor.leboran@usc.es
 
-
-./runsimular_paral -2 0 300 "./Madrid/ParInit_Optimos_08_006_Exp4.txt" Madrid
-
-
+echo "Actualizando los datos para Madrid"
+./runsimular_paral -2 0 600 "./Madrid/ParInit_Optimos_08_006_Exp4.txt" Madrid | tee ./logsSimulacion/Madrid_600R.log
+echo " Actualizando los datos para el Pais Vasco"
+./runsimular_paral -2 0 600 "./Vasco/ParInit_Optimos_08_006_Exp4.txt" Vasco | tee ./logsSimulacion/PaisVasco_600R.log
