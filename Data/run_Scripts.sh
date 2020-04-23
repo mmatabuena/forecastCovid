3e07bb4aa69058e408f1d6b5a25d14f69d5c3a39
mmatabuena <46138526+mmatabuena@users.noreply.github.com>
2020-04-23 17:32:03 +0200

    Delete run_Scripts.sh

--- Data/run_Scripts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script del shell realizado por Victor Leboran
-# Para mas info contacta en victor.leboran@usc.es
-
-echo "Actualizando los datos para Madrid"
-./runsimular_paral -2 0 600 "./Madrid/ParInit_Optimos_08_006_Exp4.txt" Madrid | tee ./logsSimulacion/Madrid_600R.log
-echo " Actualizando los datos para el Pais Vasco"
-./runsimular_paral -2 0 600 "./Vasco/ParInit_Optimos_08_006_Exp4.txt" Vasco | tee ./logsSimulacion/PaisVasco_600R.log
