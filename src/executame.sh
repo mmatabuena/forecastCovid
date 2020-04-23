e2bdf38003c92a90797add88c4a728805d87db8e
carmeiga <carmeiga97@gmail.com>
2020-04-23 21:30:02 +0200

    Correccións automatización

--- src/executame.sh
+++ src/executame.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-'Executando o código de simulación para as autonomías'
-nArchivoParam="../Data/Galicia/ParInit_Optimos_Exp6.txt"
+
+nArchivoParam="ParInit_Optimos_Exp6.txt"
 
 # Se ejecutan todas las autonomías con esos parámetros ...
-./runsimular_paral -2 0 1000 $nArchivoParam "Galicia" | tee ./Results/Ejemplo_Galicia.txt
-./runsimular_paral -2 0 1000 $nArchivoParam "Cataluna" | tee ./Results/Ejemplo_Cataluna.txt
-./runsimular_paral -2 0 1000 $nArchivoParam "Leon" | tee ./Results/Ejemplo_Leon.txt
-./runsimular_paral -2 0 1000 $nArchivoParam "Vasco" | tee ./Results/Ejemplo_Vasco.txt
-./runsimular_paral -2 0 1000 $nArchivoParam "Madrid" | tee ./Results/Ejemplo_Madrid.txt
+./runsimular_paral 25 0 200 ../Data/Galicia/$nArchivoParam "Galicia" | tee ./Results/Ejemplo_Galicia.txt
+./runsimular_paral 25 0 200 ../Data/Cataluna/$nArchivoParam "Cataluna" | tee ./Results/Ejemplo_Cataluna.txt
+./runsimular_paral 25 0 200 ../Data/Leon/$nArchivoParam "Leon" | tee ./Results/Ejemplo_Leon.txt
+./runsimular_paral 25 0 200 ../Data/Vasco/$nArchivoParam "Vasco" | tee ./Results/Ejemplo_Vasco.txt
+./runsimular_paral 25 0 200 ../Data/Madrid/$nArchivoParam "Madrid" | tee ./Results/Ejemplo_Madrid.txt
+
 
 # Se ejecutan los scripts de R que generan las gráficas
 R -e "rmarkdown::render('../Rmds/res_vasco.Rmd',output_file='../Rmds/vasco/main.html')"
