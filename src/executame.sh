87aedeb7230ad782fa82c4b0787d72ff7ac5e2ae
mmatabuena <marcos.matabuena@usc.es>
2020-04-23 17:04:15 +0200

    update

--- src/executame.sh
+++ src/executame.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 'Executando o código de simulación para as autonomías'
-./runsimular_paral -2 0 1000 "../cluster_data/Galicia/ParInit_Optimos_Exp6.txt" "Galicia" | tee Ejemplo_Galicia.txt
-./runsimular_paral -2 0 1000 "../cluster_data/Cataluna/ParInit_Optimos_Exp6.txt" "Cataluna" | tee Ejemplo_Cataluna.txt
-./runsimular_paral -2 0 1000 "../cluster_data/Leon/ParInit_Optimos_Exp6.txt "Leon" | tee Ejemplo_Leon.txt
-./runsimular_paral -2 0 1000 "../cluster_data/Vasco/ParInit_Optimos_Exp6.txt" "Vasco" | tee Ejemplo_Vasco.txt
-./runsimular_paral -2 0 1000 "../cluster_data/Madrid/ParInit_Optimos_Exp6.txt" "Madrid" | tee Ejemplo_Madrid.txt
+./runsimular_paral -2 0 1000 "../Data/Galicia/ParInit_Optimos_Exp6.txt" "Galicia" | tee Ejemplo_Galicia.txt
+./runsimular_paral -2 0 1000 "../Data/Cataluna/ParInit_Optimos_Exp6.txt" "Cataluna" | tee Ejemplo_Cataluna.txt
+./runsimular_paral -2 0 1000 "../Data/Leon/ParInit_Optimos_Exp6.txt" "Leon" | tee Ejemplo_Leon.txt
+./runsimular_paral -2 0 1000 "../Data/Vasco/ParInit_Optimos_Exp6.txt" "Vasco" | tee Ejemplo_Vasco.txt
+./runsimular_paral -2 0 1000 "../Data/Madrid/ParInit_Optimos_Exp6.txt" "Madrid" | tee Ejemplo_Madrid.txt
 
 
 R -e "rmarkdown::render('../Rmds/res_vasco.Rmd',output_file='../Rmds/vasco/main.html')"
