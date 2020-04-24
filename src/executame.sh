2fffd35898936a40efd99e94e8383ecfbbf8f11c
carmeiga <carmeiga97@gmail.com>
2020-04-24 14:28:07 +0200

    First graphs

--- src/executame.sh
+++ src/executame.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
+nArchivoParam="ParInit_Optimos_Exp7.txt"
+c=0
 
-nArchivoParam="ParInit_Optimos_Exp6.txt"
+## Declaración del array de comunidades para lanzar los scripts
+declare -a arr=("Galicia" "Cataluna" "Leon" "Vasco" "Madrid" "Navarra")
 
-# Se ejecutan todas las autonomías con esos parámetros ...
-./runsimular_paral 25 0 200 ../Data/Galicia/$nArchivoParam "Galicia" | tee ./Results/Ejemplo_Galicia.txt
-./runsimular_paral 25 0 200 ../Data/Cataluna/$nArchivoParam "Cataluna" | tee ./Results/Ejemplo_Cataluna.txt
-./runsimular_paral 25 0 200 ../Data/Leon/$nArchivoParam "Leon" | tee ./Results/Ejemplo_Leon.txt
-./runsimular_paral 25 0 200 ../Data/Vasco/$nArchivoParam "Vasco" | tee ./Results/Ejemplo_Vasco.txt
-./runsimular_paral 25 0 200 ../Data/Madrid/$nArchivoParam "Madrid" | tee ./Results/Ejemplo_Madrid.txt
+## Se recorren todos los elementos del array de Comunidades
+for varRegion in "${arr[@]}"
+do
+    
+    FILE_PARAM[c]="../Data/$varRegion/$nArchivoParam"
+    #echo "  -> Lanzando Scripts de C++ y R en <$varRegion> con <${FILE_PARAM[c]}>"
+    # Se comprueba que existen los archivos base ...
+    if [ ! -f ${FILE_PARAM[c]} ];
+    then
+        echo "  -> Se ha ignorado <$varRegion>, no está disponible el archivo <$FILE_PARAM>.."
+    else
+        echo "  -> Lanzando Scripts de C++ y R en <$varRegion>..."
+        ./runsimular_paral 60 0 200 ${FILE_PARAM[c]} $varRegion > ./Results/Ejemplo_$varRegion.txt 
+        #R -e rmarkdown::render("../Rmds/res_$varRegion.Rmd",output_file="../Rmds/$varRegion/main.html")
+
+    fi
+    c=$(($c+1));
+  
+done
+
+
 
 
 # Se ejecutan los scripts de R que generan las gráficas
-R -e "rmarkdown::render('../Rmds/res_vasco.Rmd',output_file='../Rmds/vasco/main.html')"
+#R -e "rmarkdown::render('../Rmds/res_vasco.Rmd',output_file='../Rmds/vasco/main.html')"
 R -e "rmarkdown::render('../Rmds/res_madrid.Rmd',output_file='../Rmds/madrid/main.html')"
-R -e "rmarkdown::render('../Rmds/res_cataluna.Rmd',output_file='../Rmds/cataluna/main.html')"
-R -e "rmarkdown::render('../Rmds/res_leon.Rmd',output_file='../Rmds/leon/main.html')"
+#R -e "rmarkdown::render('../Rmds/res_cataluna.Rmd',output_file='../Rmds/cataluna/main.html')"
+#R -e "rmarkdown::render('../Rmds/res_leon.Rmd',output_file='../Rmds/leon/main.html')"
 R -e "rmarkdown::render('../Rmds/res_galicia.Rmd',output_file='../Rmds/galicia/main.html')"
