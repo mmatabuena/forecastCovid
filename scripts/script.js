0699e3bfe66731ed82d67c9aac771bf3a28cb727
Alblahm <alblahm@hotmail.com>
2020-04-26 14:43:20 +0200

    update charts

--- scripts/script.js
+++ scripts/script.js
@@ -20,6 +20,7 @@ function updateTexts(){
   $('#div03').text(language.Cabecera03);
   $('#div04').text(language.Pie01);
   $('#div05').text(language.Pie02);	
+  $('#div06').text(language.Cabecera04);
 }
 
 $(document).ready(function(){
