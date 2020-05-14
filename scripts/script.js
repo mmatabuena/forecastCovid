4b8ab403a44098c83a9e6d45e2bf78ddaab8f8ba
Alblahm <alblahm@hotmail.com>
2020-05-14 09:50:47 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -48,14 +48,14 @@ function updateTexts(){
 }
 
 function updateImages(){
-  $('#Imagen_I1').prop('src','./Rmds/Espana/' + localStorage.getItem('selRegion') + '/Image_I1.png');
-  $('#Imagen_I2').prop('src','./Rmds/Espana/' + localStorage.getItem('selRegion') + '/Image_I2.png');
-  $('#Imagen_I3').prop('src','./Rmds/Espana/' + localStorage.getItem('selRegion') + '/Image_I3.png');
-  $('#Imagen_M').prop('src','./Rmds/Espana/' + localStorage.getItem('selRegion') + '/Image_M.png');  
-  $('#Imagen_R1').prop('src','./Rmds/Espana/' + localStorage.getItem('selRegion') + '/Image_R1.png');
-  $('#Imagen_R2').prop('src','./Rmds/Espana/' + localStorage.getItem('selRegion') + '/Image_R2.png');
-  $('#Imagen_T').prop('src','./Rmds/Espana/' + localStorage.getItem('selRegion') + '/Image_T.png');
-  $('#Imagen_TR').prop('src','./Rmds/Espana/' + localStorage.getItem('selRegion') + '/Image_TR.png'); 
+  $('#Imagen_I1').prop('src','./Rmds/Espana/' + localStorage.getItem('selRegion') + '/Image_I1.jpg');
+  $('#Imagen_I2').prop('src','./Rmds/Espana/' + localStorage.getItem('selRegion') + '/Image_I2.jpg');
+  $('#Imagen_I3').prop('src','./Rmds/Espana/' + localStorage.getItem('selRegion') + '/Image_I3.jpg');
+  $('#Imagen_M').prop('src','./Rmds/Espana/' + localStorage.getItem('selRegion') + '/Image_M.jpg');  
+  $('#Imagen_R1').prop('src','./Rmds/Espana/' + localStorage.getItem('selRegion') + '/Image_R1.jpg');
+  $('#Imagen_R2').prop('src','./Rmds/Espana/' + localStorage.getItem('selRegion') + '/Image_R2.jpg');
+  $('#Imagen_T').prop('src','./Rmds/Espana/' + localStorage.getItem('selRegion') + '/Image_T.jpg');
+  $('#Imagen_TR').prop('src','./Rmds/Espana/' + localStorage.getItem('selRegion') + '/Image_TR.jpg'); 
   $('#aRefImagen_I1').prop('href','./Rmds/Espana/' + localStorage.getItem('selRegion') + '/Image_I1.png');
   $('#aRefImagen_I2').prop('href','./Rmds/Espana/' + localStorage.getItem('selRegion') + '/Image_I2.png');
   $('#aRefImagen_I3').prop('href','./Rmds/Espana/' + localStorage.getItem('selRegion') + '/Image_I3.png');
