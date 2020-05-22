d77076234a2cd8e73a09f6ae75e9814d9f84db76
Alblahm <alblahm@hotmail.com>
2020-05-22 03:27:28 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -50,23 +50,23 @@ function updateTexts(){
 }
 
 function updateImages(){
-  $('#Imagen_I1').prop('src','./Rmds/' + localStorage.getItem('selRegion') + '/' + localStorage.getItem('selRegion') + '/Image_I1.jpg');
-  $('#Imagen_I2').prop('src','./Rmds/' + localStorage.getItem('selRegion') + '/' + localStorage.getItem('selRegion') + '/Image_I2.jpg');
-  $('#Imagen_I3').prop('src','./Rmds/' + localStorage.getItem('selRegion') + '/' + localStorage.getItem('selRegion') + '/Image_I3.jpg');
-  $('#Imagen_M').prop('src','./Rmds/' + localStorage.getItem('selRegion') + '/' + localStorage.getItem('selRegion') + '/Image_M.jpg');  
-  $('#Imagen_R1').prop('src','./Rmds/' + localStorage.getItem('selRegion') + '/' + localStorage.getItem('selRegion') + '/Image_R1.jpg');
-  $('#Imagen_R2').prop('src','./Rmds/' + localStorage.getItem('selRegion') + '/' + localStorage.getItem('selRegion') + '/Image_R2.jpg');
-  $('#Imagen_T').prop('src','./Rmds/' + localStorage.getItem('selRegion') + '/' + localStorage.getItem('selRegion') + '/Image_T.jpg');
-  $('#Imagen_TR').prop('src','./Rmds/' + localStorage.getItem('selRegion') + '/' + localStorage.getItem('selRegion') + '/Image_TR.jpg'); 
-  $('#aRefImagen_I1').prop('href','./Rmds/' + localStorage.getItem('selRegion') + '/' + localStorage.getItem('selRegion') + '/Image_I1.png');
-  $('#aRefImagen_I2').prop('href','./Rmds/' + localStorage.getItem('selRegion') + '/' + localStorage.getItem('selRegion') + '/Image_I2.png');
-  $('#aRefImagen_I3').prop('href','./Rmds/' + localStorage.getItem('selRegion') + '/' + localStorage.getItem('selRegion') + '/Image_I3.png');
-  $('#aRefImagen_M').prop('href','./Rmds/' + localStorage.getItem('selRegion') + '/' + localStorage.getItem('selRegion') + '/Image_M.png');  
-  $('#aRefImagen_R1').prop('href','./Rmds/' + localStorage.getItem('selRegion') + '/' + localStorage.getItem('selRegion') + '/Image_R1.png');
-  $('#aRefImagen_R2').prop('href','./Rmds/' + localStorage.getItem('selRegion') + '/' + localStorage.getItem('selRegion') + '/Image_R2.png');
-  $('#aRefImagen_T').prop('href','./Rmds/' + localStorage.getItem('selRegion') + '/' + localStorage.getItem('selRegion') + '/Image_T.png');
-  $('#aRefImagen_TR').prop('href','./Rmds/' + localStorage.getItem('selRegion') + '/' + localStorage.getItem('selRegion') + '/Image_TR.png'); 
-  $('#Link_InfoExt').prop('href','./Rmds/' + localStorage.getItem('selRegion') + '/' + localStorage.getItem('selRegion') + '/main.html');
+  $('#Imagen_I1').prop('src','./Rmds/' + localStorage.getItem('selCountry') + '/' + localStorage.getItem('selRegion') + '/Image_I1.jpg');
+  $('#Imagen_I2').prop('src','./Rmds/' + localStorage.getItem('selCountry') + '/' + localStorage.getItem('selRegion') + '/Image_I2.jpg');
+  $('#Imagen_I3').prop('src','./Rmds/' + localStorage.getItem('selCountry') + '/' + localStorage.getItem('selRegion') + '/Image_I3.jpg');
+  $('#Imagen_M').prop('src','./Rmds/' + localStorage.getItem('selCountry') + '/' + localStorage.getItem('selRegion') + '/Image_M.jpg');  
+  $('#Imagen_R1').prop('src','./Rmds/' + localStorage.getItem('selCountry') + '/' + localStorage.getItem('selRegion') + '/Image_R1.jpg');
+  $('#Imagen_R2').prop('src','./Rmds/' + localStorage.getItem('selCountry') + '/' + localStorage.getItem('selRegion') + '/Image_R2.jpg');
+  $('#Imagen_T').prop('src','./Rmds/' + localStorage.getItem('selCountry') + '/' + localStorage.getItem('selRegion') + '/Image_T.jpg');
+  $('#Imagen_TR').prop('src','./Rmds/' + localStorage.getItem('selCountry') + '/' + localStorage.getItem('selRegion') + '/Image_TR.jpg'); 
+  $('#aRefImagen_I1').prop('href','./Rmds/' + localStorage.getItem('selCountry') + '/' + localStorage.getItem('selRegion') + '/Image_I1.png');
+  $('#aRefImagen_I2').prop('href','./Rmds/' + localStorage.getItem('selCountry') + '/' + localStorage.getItem('selRegion') + '/Image_I2.png');
+  $('#aRefImagen_I3').prop('href','./Rmds/' + localStorage.getItem('selCountry') + '/' + localStorage.getItem('selRegion') + '/Image_I3.png');
+  $('#aRefImagen_M').prop('href','./Rmds/' + localStorage.getItem('selCountry') + '/' + localStorage.getItem('selRegion') + '/Image_M.png');  
+  $('#aRefImagen_R1').prop('href','./Rmds/' + localStorage.getItem('selCountry') + '/' + localStorage.getItem('selRegion') + '/Image_R1.png');
+  $('#aRefImagen_R2').prop('href','./Rmds/' + localStorage.getItem('selCountry') + '/' + localStorage.getItem('selRegion') + '/Image_R2.png');
+  $('#aRefImagen_T').prop('href','./Rmds/' + localStorage.getItem('selCountry') + '/' + localStorage.getItem('selRegion') + '/Image_T.png');
+  $('#aRefImagen_TR').prop('href','./Rmds/' + localStorage.getItem('selCountry') + '/' + localStorage.getItem('selRegion') + '/Image_TR.png'); 
+  $('#Link_InfoExt').prop('href','./Rmds/' + localStorage.getItem('selCountry') + '/' + localStorage.getItem('selRegion') + '/main.html');
   $('#Link_Github').prop('text',language.Pie03); 
 }
 
